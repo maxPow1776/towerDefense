2e96a5d9223ddc452838c7d18a6c41774875c567
Максим <maks.greb2@gmail.com>
2020-04-13 13:08:02 +0300

    Враги бьют главную башню не бесконечно, а умирают после нанесения 1 удара

--- Assets/Scripts/ConditionCollision.cs
+++ Assets/Scripts/ConditionCollision.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class ConditionCollision : MonoBehaviour
 {
     private string _isFight = "isFight";
+    private string _isDie = "isDie";
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<TowerFighter>() || collision.gameObject.GetComponent<MainTower>())
+        if (collision.gameObject.GetComponent<TowerFighter>())
         {
             GetComponent<AutoMove>()._isCollision = true;
             gameObject.GetComponent<EnemyFighter>().StartFight(collision.gameObject);
             gameObject.GetComponent<Animator>().SetBool(_isFight, true);
         }
+        if (collision.gameObject.GetComponent<MainTower>())
+        {
+            GetComponent<AutoMove>()._isCollision = true;
+            gameObject.GetComponent<EnemyFighter>().StartFight(collision.gameObject);
+            gameObject.GetComponent<Animator>().SetBool(_isFight, true);
+            StartCoroutine(FightWithMainTower());
+        }
+    }
+
+    IEnumerator FightWithMainTower()
+    {
+        yield return new WaitForSeconds(2.5f);
+        gameObject.GetComponent<Animator>().SetBool(_isDie, true);
+        Destroy(gameObject, 0.8f);
     }
 }
