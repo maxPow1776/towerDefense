0f4872b23934453eee4694967e763d2a3c341637
Максим <maks.greb2@gmail.com>
2020-04-27 15:13:53 +0300

    Поправил структуру кода

--- Assets/Scripts/AbstractFighter.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class AbstractFighter : MonoBehaviour
-{
-    public int Health;
-    public int Protection;
-    public int Damage;
-    public GameObject Hp;
-}
